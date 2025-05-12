7904321a2f6488ee4c30e39411b695982d823b7f
Malibongwe <ap.mshumpela@gmail.com>
2025-05-12 14:05:53 +0200

    Updated read Properties

--- Utils/readProperties_Login.py
+++ Utils/readProperties_Login.py
@@ -1,10 +1,19 @@
 import configparser
 
-config = configparser.RawConfigParser
+config = configparser.RawConfigParser()
 config.read("./Configuration/Login.ini")
 
 
 class ReadConfig_Login():
 
+    @staticmethod
     def getUrl(self):
-     return config.get(" "," ")
+        return config.get("Common Details", "url")
+
+    @staticmethod
+    def getUsername(self):
+        return config.get("Login Details", "username")
+
+    @staticmethod
+    def getPassword(self):
+        return config.get("Login Details", "password")
